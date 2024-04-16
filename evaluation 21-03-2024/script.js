fe9eef16008720e98b75d73f902ae2d297bf92d3
Christopheantoine-mao <160973976+Christopheantoine-mao@users.noreply.github.com>
2024-04-16 10:59:21 +0200

    Add files via upload

---
+++ evaluation 21-03-2024/script.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+    // Carousel Functionality
+    let slideIndex = 0;
+
+    function showSlides() {
+        let slides = $(".item");
+        slides.hide(); // Hide all slides
+        slideIndex = (slideIndex < 0) ? slides.length - 1 : slideIndex % slides.length;
+        slides.eq(slideIndex).show(); // Show the current slide
+    }
+
+    $(".next").click(function() {
+        slideIndex++;
+        showSlides();
+    });
+
+    $(".prev").click(function() {
+        slideIndex--;
+        showSlides();
+    });
+
+    showSlides(); // slide apparante 
+
+    // fonction de la galerie
+    $('#galerie img').click(function() {
+        ouvrirEnGrand(this.src, this.alt); //  click ouverture grande image lors du clique sur la photo
+    });
+
+    // Assuming you dynamically add the close button along with the enlarged image
+    // Therefore, use event delegation for dynamically added elements
+    $('body').on('click', '.close-button', function() {
+        $('#imageSelectionnee').hide(); // Hide the enlarged image container
+    });
+
+    // Function to display an image in a larger view
+    function ouvrirEnGrand(src, alt) {
+        // Display the image in the 'imageSelectionnee' container
+        var enlargedHTML = `<div class="close-button" style="cursor:pointer; position:absolute; top:10px; right:10px; font-size:24px; color:white; background-color:black; border-radius:50%;">×</div><img src="${src}" alt="${alt}" style="width:100%; height:auto;">`;
+        document.getElementById('imageSelectionnee').innerHTML = enlargedHTML;
+        $('#imageSelectionnee').css({'display': 'flex', 'justify-content': 'center', 'align-items': 'center'}); // Adjust CSS for centering
+    }
+});
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleButton = document.querySelector('.menu-toggle');
+    const navbarLinks = document.querySelector('.navbar-links');
+
+    toggleButton.addEventListener('click', function() {
+        navbarLinks.classList.toggle('active');
+    });
+});
+
+
+//scroll
+window.onscroll = function() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("scrollIndicator").style.width = scrolled + "%";
+};
+
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0; 
+}
+
+function scrollToBottom() {
+    document.body.scrollTop = document.body.scrollHeight; 
+    document.documentElement.scrollTop = document.documentElement.scrollHeight; 
+}
